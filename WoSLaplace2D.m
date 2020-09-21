c850a342f13fd67a573d19d49bba24142d456475
lamminade <leah.dawson@mail.mcgill.ca>
2020-09-21 18:39:57 -0400

    Vectorized some stuff

--- WoSLaplace2D.m
+++ WoSLaplace2D.m
@@ -13,12 +13,14 @@ scene = [
 %%%%%%%%%%%%%%%% main %%%%%%%%%%%%%%%%
 N = 129;    % image size
 out = zeros(N,N);
+out2 = zeros(N,N);
+
 for j = 1:N
-    fprintf("row %i of %i\n", j, s);
+    fprintf("row %i of %i\n", j, N);
     for i = 1:N
         x0 = [ (i-1)./(N-1), (j-1)./(N-1) ];
-        u = solve(x0, scene);
-        out(i,j) = cast(u, 'double');
+        u = laplaceSolve(x0, scene);
+        out(i,j) = u;
     end
 end
 
@@ -26,78 +28,64 @@ imagesc(out);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-
-
-
-% returns the point on segment s closest to x
-function y1 = closestPoint(x, s) % s = [ x1 y1 x2 y2], s[0] = [s(1) s(2)] s[1] = [s(3) s(4)]
+function y1 = closestPoints(xv, s) % s = [ x1 y1 x2 y2], s[0] = [s(1) s(2)] s[1] = [s(3) s(4)]
+% closestPoints - 
+% returns an array of points on s which are closest
+% to the values on x -- actually probably didnt need to vectorize this
+% whoops...
+%
     u = [(s(3)-s(1)) (s(4)-s(2))];
-    dotOne = dot(x - [s(1) s(2)], u);   
-    dotTwo = dot(u,u);
-    t = clamp(dotOne./dotTwo);
+    
+    ulong = repmat(u, size(xv,1), 1);
+    slong = repmat([s(1) s(2)], size(xv,1), 1);
+    
+    dotOne = dot(xv - slong, ulong, 2);             % dot(xv - slong, ulong);   
+    dotTwo = dot(ulong, ulong, 2);                  % f
+    
+    t = clampVector(dotOne./dotTwo, 0, 1);
     y1 = (1-t)*([s(1) s(2)]) + t*([s(3) s(4)]);
 end
 
-% random number in a range - RAND_MAX is like the C variable
-function r = myRandom(min, max)
-    RAND_MAX = 32767.0;
-    rRandMax = 1.0 ./ RAND_MAX;
-    u = rRandMax .* (rand() .* RAND_MAX);
-    r = (u .* (max - min)) + min;
-end
 
-% solves a Laplace equation Δu = 0 at x0, where the boundary is given
-% by a collection of segments, and the boundary conditions are given
-% by a function g that can be evaluated at any point in space
-function res = solve(x0, segments)
+function res = laplaceSolve(x0, segments)
+% laplaceSolve  Laplace Method
+% Solves a Laplace equation Δu = 0 at x0, where the boundary is given
+%   by a collection of segments, and the boundary conditions are 
+%   evaluated by the function 'checker' which can be evaluated at any point in space
+%
     eps = 0.01;     % stopping tolerance
     nWalks = 128;   % number of Monte Carlo samples
     maxSteps = 16;  % maximum walk length
     
-    sum = 0;
-    
     % can we do all walks at once...   need to vectorize some of the helper
     % functions.
    
-%     xv = ones(128,1) * x0;
-%     steps = 0;
-%     while true
-%         R = ones(128,1) * realmax();
-%         for j = 1:size(segments,1)
-%             % get closest points simulateously!
-%             
-%         end
-%     end
-    
-    
-    for i = 0:nWalks
-        x = x0;
-        steps = 0;
-       
-        
-        while true % this is my do-while in matlab
-            R = realmax();
-            for j = 1:size(segments, 1)                     % # of rows
-                % we want the row j of segments
-                p = closestPoint(x, segments(j:j,:));   
-                R = min(R, norm((x-p)));
-            end
-            theta = myRandom(0, 2.*pi);
-            x = x + [R.*cos(theta), R.*sin(theta)];
-            steps = steps + 1;
-            
-            if (R < eps || steps > maxSteps)
-                break
-            end
+    xv = ones(nWalks,1) * x0;
+    steps = 0;
+    while true
+        R = ones(nWalks,1) * realmax();
+        for j = 1:size(segments,1)
+            % get closest points simulateously!
+            pv = closestPoints(xv, segments(j:j,:));
+            R = min(R, vecnorm((xv-pv).').');
         end
+        theta = myRandomVect(nWalks, 0, 2.*pi);
+        xv = xv + [R.*cos(theta), R.*sin(theta)];
+        steps = steps + 1;
         
-        sum = sum + checker(x);
+        if (steps > maxSteps)
+            break
+        end
     end
-    res = sum ./ nWalks; % monte carlo estimate
+    
+    sumMat = arrayfun(@(x,y) checker([x y]), xv(:,1:1), xv(:,2:2)); %% or other boundary condition thing
+    res = sum(sumMat) ./ nWalks; % monte carlo estimate
 end
 
+% boundary condition checker
+% the one from the sample code 
 function c = checker(x)
-    s = 12;
+    s = 6;
     c = fmod(floor(s .* x(1)) + floor(s .* x(2)), 2 );
 end
 
@@ -107,8 +95,10 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % performs c-like clamp function
-function y = clamp(v)
-    y = min(max(v, 0), 1);
+function y = clampVector(v, lo, hi)
+    y = v;
+    y(y>hi) = hi;
+    y(y<lo) = lo;
 end
 
 % performs c-like fmod
@@ -122,5 +112,12 @@ function m = fmod(a, b)
     else
         m = mod(a, b) + (b .* (sign(a) - 1) ./ 2);
     end
+end
+
+function rv = myRandomVect(n, min, max)
+    RAND_MAX = 32767;
+    rRandMax = 1 ./ RAND_MAX;
+    u = rRandMax * (rand(n, 1) * RAND_MAX);
+    rv = (u * (max - min)) + min;
 end
 
